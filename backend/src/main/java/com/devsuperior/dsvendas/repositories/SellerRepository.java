8c99c8e7722c83a609da5d254697e383d7a8c95d
carlacou <carlacoutinhopontes@gmail.com>
2021-09-12 00:16:25 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsvendas.entities.Seller;
 
+
+
+
 public interface SellerRepository extends JpaRepository<Seller, Long> {
 
 }
